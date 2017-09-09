3cdde1a13d2d174a8dd2196a10c40fad2d64817f
VoiceBeer <370396850@qq.com>
2017-09-09 16:28:51 +0800

    Add a ViewControllerLifecycle logging file

--- MyFaceDemo/FaceViewController.swift
+++ MyFaceDemo/FaceViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FaceViewController: UIViewController {
+class FaceViewController: VCLLoggingViewController {
 
 //    override func viewDidLoad() {
 //        super.viewDidLoad()

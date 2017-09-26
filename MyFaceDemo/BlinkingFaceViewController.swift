7bb6a32a3d486ef832783fc85e2e47149c9dc87d
VoiceBeer <370396850@qq.com>
2017-09-26 11:43:59 +0800

    Add Timer

---
+++ MyFaceDemo/BlinkingFaceViewController.swift
@@ -0,0 +1,43 @@
+//
+//  BlinkingFaceViewController.swift
+//  MyFaceDemo
+//
+//  Created by VoiceBeer on 2017/9/26.
+//  Copyright © 2017年 VoiceBeer. All rights reserved.
+//
+
+import UIKit
+
+class BlinkingFaceViewController: FaceViewController {
+    var blinking = false {
+        didSet {
+            blinkIfNeeded()
+        }
+    }
+    
+    private struct BlinkRate {
+        static let closedDuration: TimeInterval = 0.4
+        static let openDuration: TimeInterval = 2.5
+    }
+    
+    private func blinkIfNeeded() {
+        if blinking {
+            faceView.eyesOpen = false
+            Timer.scheduledTimer(withTimeInterval: BlinkRate.closedDuration, repeats: false) { [weak self] timer in
+                self?.faceView.eyesOpen = true
+                Timer.scheduledTimer(withTimeInterval: BlinkRate.openDuration, repeats: false) { [weak self] timer in
+                    self?.blinkIfNeeded()
+                }
+            }
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        blinking = true
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        blinking = false
+    }
+}
